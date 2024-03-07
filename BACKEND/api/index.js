510ff82955883f65e4d8feda29e101d64c7692d1
parth sharma <ps13092007@gmail.com>
2024-03-07 14:50:22 +0530

    server file update

--- BACKEND/api/index.js
+++ BACKEND/api/index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import db from "../config/database.js";
-import User from "../models/User.js";
+import db from ".././config/database.js";
+import User from "./../models/User.js";
 
 const app = express();
 dotenv.config({ path: "./config/.env" });
@@ -36,9 +36,9 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 
-import auth from "../routes/auth/auth.js";
-import blog from "../routes/blogs/blog.js";
-import dashboard from "../routes/dashboard/dashboard.js";
+import auth from "./../routes/auth/auth.js";
+import blog from "./../routes/blogs/blog.js";
+import dashboard from "./../routes/dashboard/dashboard.js";
 
 // auth api
 app.use("/auth/api/v1/", auth);
