72f7dbc0834907098e4f630225026f295748ab96
parth sharma <ps13092007@gmail.com>
2024-03-07 13:44:35 +0530

    orogin

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true, limit: "18kb" }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "https://blogify-delta.vercel.app/",
+  origin: "https://blogify-sigma.vercel.app/",
   optionsSuccessStatus: 200,
   credentials: true,
    methods: 'GET, POST, PUT, DELETE'
