d04e76af275c14f0412ba3cb49786c643dbed3ac
parth sharma <ps13092007@gmail.com>
2024-03-07 12:09:55 +0530

    env file fix

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,4 +5,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    process.env: {
+      DATABASE_BASE_URL: process.env.DATABASE_BASE_URL,
+      RTE_API_KEY: process.env.RTE_API_KEY,
+    }
+  }
 });
