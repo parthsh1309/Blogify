17803a38613981befbcdc7750a84ac23d9add23c
parth sharma <ps13092007@gmail.com>
2024-03-09 20:02:31 +0530

    origin updated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true, limit: "18kb" }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "https://blogify-sigma.vercel.app/",
+  origin: "https://blogify-sigma.vercel.app",
   optionsSuccessStatus: 200,
   credentials: true,
 };
